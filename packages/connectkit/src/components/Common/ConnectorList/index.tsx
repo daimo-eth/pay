72064c870e3b218a2a782d5e8946837040fa995a
Gianluca Minoprio <gianlucaminoprio@icloud.com>
2025-06-10 21:12:25 -0700

    fix deposit injected connector mobile

--- packages/connectkit/src/components/Common/ConnectorList/index.tsx
+++ packages/connectkit/src/components/Common/ConnectorList/index.tsx
@@ -1,3 +1,4 @@
+import { type Connector } from "wagmi";
 import { ROUTES } from "../../../constants/routes";
 import { useConnect } from "../../../hooks/useConnect";
 import { useDaimoPay } from "../../../hooks/useDaimoPay";
@@ -100,13 +101,17 @@ const ConnectorItem = ({
     (detectBrowser() === "safari" || detectBrowser() === "ios") &&
     isCoinbaseWalletConnector(wallet.connector?.id);
 
-  const onClick = () => {
+  const onClick = (connector?: Connector) => {
     if (redirectToMoreWallets) {
       context.setRoute(ROUTES.MOBILECONNECTORS);
-    } else if (context.paymentState.isDepositFlow && isMobile) {
+    } else if (context.paymentState.isDepositFlow && isMobile && !connector) {
       context.paymentState.setSelectedWallet(wallet);
       context.setRoute(ROUTES.SELECT_WALLET_AMOUNT);
-    } else if (isMobile && wallet.getDaimoPayDeeplink != null) {
+    } else if (
+      isMobile &&
+      wallet.getDaimoPayDeeplink != null &&
+      !wallet.connector
+    ) {
       context.paymentState.openInWalletBrowser(wallet);
     } else {
       if (shouldConnectImmediately) {
@@ -118,13 +123,7 @@ const ConnectorItem = ({
   };
 
   return (
-    <ConnectorButton
-      type="button"
-      // Is this <a> tag necessary?
-      // as={deeplink ? "a" : undefined}
-      // href={deeplink ? deeplink : undefined}
-      onClick={onClick}
-    >
+    <ConnectorButton type="button" onClick={() => onClick(wallet?.connector)}>
       <ConnectorIcon
         data-small={wallet.iconShouldShrink}
         data-shape={wallet.iconShape}

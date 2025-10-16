5e4c4d6bb0c00e76239d38454e8b41c22cfe3d7b
Andrew <andrewliu08@gmail.com>
2025-10-16 09:59:02 -0700

    export world sign in hook

--- packages/connectkit/src/world.ts
+++ packages/connectkit/src/world.ts
@@ -1,4 +1,5 @@
 export { useWithdrawToWorld } from "./world-mini-app/useWithdrawToWorld";
+export { useWorldSignIn } from "./world-mini-app/useWorldSignIn";
 export { WorldPayButton } from "./world-mini-app/WorldPayButton";
 export type {
   WorldPayButtonCustomProps,

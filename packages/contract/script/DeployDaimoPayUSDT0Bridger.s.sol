926edda907376b1190bc90708b9664b8f9015737
Andrew <andrewliu08@gmail.com>
2025-10-26 18:18:40 -0700

    disable usdt0 deploy script

--- packages/contract/script/DeployDaimoPayUSDT0Bridger.s.sol
+++ packages/contract/script/DeployDaimoPayUSDT0Bridger.s.sol
@@ -1,51 +1,53 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.13;
+// Reenable once USDT0 bridge routes are added
 
-import "forge-std/Script.sol";
+// // SPDX-License-Identifier: GPL-3.0-or-later
+// pragma solidity ^0.8.13;
 
-import "../src/DaimoPayUSDT0Bridger.sol";
-import "./constants/USDT0BridgeRouteConstants.sol";
-import "./constants/Constants.s.sol";
+// import "forge-std/Script.sol";
 
-bytes32 constant DEPLOY_SALT_USDT0_BRIDGER = keccak256(
-    "DaimoPayUSDT0Bridger-deploy1"
-);
+// import "../src/DaimoPayUSDT0Bridger.sol";
+// import "./constants/USDT0BridgeRouteConstants.sol";
+// import "./constants/Constants.s.sol";
 
-contract DeployDaimoPayUSDT0Bridger is Script {
-    function run() public {
-        (
-            uint256[] memory chainIds,
-            DaimoPayLayerZeroBridger.LZBridgeRoute[] memory bridgeRoutes
-        ) = getUSDT0BridgeRoutes(block.chainid);
+// bytes32 constant DEPLOY_SALT_USDT0_BRIDGER = keccak256(
+//     "DaimoPayUSDT0Bridger-deploy1"
+// );
 
-        if (chainIds.length == 0) {
-            revert("No USDT0 bridge routes found");
-        }
+// contract DeployDaimoPayUSDT0Bridger is Script {
+//     function run() public {
+//         (
+//             uint256[] memory chainIds,
+//             DaimoPayLayerZeroBridger.LZBridgeRoute[] memory bridgeRoutes
+//         ) = getUSDT0BridgeRoutes(block.chainid);
 
-        // Log route details
-        for (uint256 i = 0; i < bridgeRoutes.length; ++i) {
-            console.log("Chain ID:", chainIds[i]);
-            console.log("Dst EID:", bridgeRoutes[i].dstEid);
-            console.log("App:", bridgeRoutes[i].app);
-            console.log("Bridge token in:", bridgeRoutes[i].bridgeTokenIn);
-            console.log("Bridge token out:", bridgeRoutes[i].bridgeTokenOut);
-            console.log("--------------------------------");
-        }
+//         if (chainIds.length == 0) {
+//             revert("No USDT0 bridge routes found");
+//         }
 
-        vm.startBroadcast();
+//         // Log route details
+//         for (uint256 i = 0; i < bridgeRoutes.length; ++i) {
+//             console.log("Chain ID:", chainIds[i]);
+//             console.log("Dst EID:", bridgeRoutes[i].dstEid);
+//             console.log("App:", bridgeRoutes[i].app);
+//             console.log("Bridge token in:", bridgeRoutes[i].bridgeTokenIn);
+//             console.log("Bridge token out:", bridgeRoutes[i].bridgeTokenOut);
+//             console.log("--------------------------------");
+//         }
 
-        address bridger = CREATE3.deploy(
-            DEPLOY_SALT_USDT0_BRIDGER,
-            abi.encodePacked(
-                type(DaimoPayUSDT0Bridger).creationCode,
-                abi.encode(chainIds, bridgeRoutes)
-            )
-        );
-        console.log("USDT0 bridger deployed at address:", address(bridger));
+//         vm.startBroadcast();
 
-        vm.stopBroadcast();
-    }
+//         address bridger = CREATE3.deploy(
+//             DEPLOY_SALT_USDT0_BRIDGER,
+//             abi.encodePacked(
+//                 type(DaimoPayUSDT0Bridger).creationCode,
+//                 abi.encode(chainIds, bridgeRoutes)
+//             )
+//         );
+//         console.log("USDT0 bridger deployed at address:", address(bridger));
 
-    // Exclude from forge coverage
-    function test() public {}
-}
+//         vm.stopBroadcast();
+//     }
+
+//     // Exclude from forge coverage
+//     function test() public {}
+// }

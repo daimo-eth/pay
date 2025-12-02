2a3c7c95ebda09a5cc6a978c58677e207493f7df
Gianluca Minoprio <gianlucaminoprio@icloud.com>
2025-12-02 12:00:01 -0800

    prioritizedWalletId daimopaybutton

--- examples/nextjs-app/src/app/deposit/page.tsx
+++ examples/nextjs-app/src/app/deposit/page.tsx
@@ -163,6 +163,7 @@ export default function DemoDeposit() {
                 printEvent(e);
                 setTxHash(e.txHash);
               }}
+              prioritizedWalletId="com.coinbase.wallet"
             />
             {txHash && (
               <TextLink

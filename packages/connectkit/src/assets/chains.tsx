dc762e2254a2747bd37d9b477e4981b316629767
andrewliu08 <55035762+andrewliu08@users.noreply.github.com>
2026-01-09 11:24:01 -0800

    hyperevm support (#186)
    * hyperevm constants
    
    * deploy salt refactor
    
    * deploy new contracts

--- packages/connectkit/src/assets/chains.tsx
+++ packages/connectkit/src/assets/chains.tsx
@@ -5,6 +5,7 @@ import {
   celo,
   ethereum,
   gnosis,
+  hyperEvm,
   linea,
   monad,
   optimism,
@@ -1137,6 +1138,27 @@ export const Monad = ({ testnet, ...props }: { testnet?: boolean }) => (
   </svg>
 );
 
+export const Hyperliquid = ({ testnet, ...props }: { testnet?: boolean }) => (
+  <svg
+    {...props}
+    width="44"
+    height="44"
+    viewBox="-24 -24 192 192"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{
+      background: testnet
+        ? "linear-gradient(180deg, #8995A9 0%, #424D5F 99.48%)"
+        : "#072723",
+    }}
+  >
+    <path
+      d="M144 71.6991C144 119.306 114.866 134.582 99.5156 120.98C86.8804 109.889 83.1211 86.4521 64.116 84.0456C39.9942 81.0113 37.9057 113.133 22.0334 113.133C3.5504 113.133 0 86.2428 0 72.4315C0 58.3063 3.96809 39.0542 19.736 39.0542C38.1146 39.0542 39.1588 66.5722 62.132 65.1073C85.0007 63.5379 85.4184 34.8689 100.247 22.6271C113.195 12.0593 144 23.4641 144 71.6991Z"
+      fill="#AEFFDB"
+    />
+  </svg>
+);
+
 const Scroll = ({ ...props }) => (
   <svg
     {...props}
@@ -1177,8 +1199,9 @@ export const chainToLogo = {
   [base.chainId]: <Base />,
   [bsc.chainId]: <BinanceSmartChain />,
   [celo.chainId]: <Celo />,
-  [gnosis.chainId]: <Gnosis />,
   [ethereum.chainId]: <Ethereum />,
+  [gnosis.chainId]: <Gnosis />,
+  [hyperEvm.chainId]: <Hyperliquid />,
   [linea.chainId]: <Linea />,
   [monad.chainId]: <Monad />,
   [optimism.chainId]: <Optimism />,

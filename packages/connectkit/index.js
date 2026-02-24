6a851bf7812d68eb0727e1a187e09f78871902bc
DC <dc@dcpos.ch>
2026-02-23 23:49:20 -0800

    fix: correct docs URL and SDK client wire format
    - Fix deprecated package error messages to use https://docs.daimo.com
    - Fix SDK client to match TRPC v11 wire format (no json wrapper)

--- packages/connectkit/index.js
+++ packages/connectkit/index.js
@@ -1,1 +1,1 @@
-throw new Error("@daimo/pay is deprecated. Use @daimo/sdk instead. See https://daimo.com/docs");
+throw new Error("@daimo/pay is deprecated. Use @daimo/sdk instead. See https://docs.daimo.com");

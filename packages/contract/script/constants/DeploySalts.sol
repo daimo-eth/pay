54f9c9e734995931ebbbe0a6457ec9eb6e7ab3ac
Andrew <andrewliu08@gmail.com>
2026-03-18 21:57:24 -0700

    deploy new bridgers

--- packages/contract/script/constants/DeploySalts.sol
+++ packages/contract/script/constants/DeploySalts.sol
@@ -30,32 +30,32 @@ bytes32 constant DEPLOY_SALT_DAIMO_PAY_PRICER = keccak256(
 
 // Bridger contracts
 bytes32 constant DEPLOY_SALT_DAIMO_PAY_BRIDGER = keccak256(
-    "DaimoPayBridger-deploy32"
+    "DaimoPayBridger-deploy34"
 );
 bytes32 constant DEPLOY_SALT_DA_BRIDGER = keccak256(
-    "DepositAddressBridger-deploy2"
+    "DepositAddressBridger-deploy4"
 );
 
 bytes32 constant DEPLOY_SALT_ACROSS_BRIDGER = keccak256(
-    "DaimoPayAcrossBridger-deploy6"
+    "DaimoPayAcrossBridger-deploy8"
 );
 bytes32 constant DEPLOY_SALT_AXELAR_BRIDGER = keccak256(
-    "DaimoPayAxelarBridger-deploy7"
+    "DaimoPayAxelarBridger-deploy9"
 );
 bytes32 constant DEPLOY_SALT_CCTP_BRIDGER = keccak256(
-    "DaimoPayCCTPBridger-deploy3"
+    "DaimoPayCCTPBridger-deploy5"
 );
 bytes32 constant DEPLOY_SALT_CCTP_V2_BRIDGER = keccak256(
-    "DaimoPayCCTPV2Bridger-deploy7"
+    "DaimoPayCCTPV2Bridger-deploy9"
 );
 bytes32 constant DEPLOY_SALT_HOP_BRIDGER = keccak256(
-    "DaimoPayHopBridger-deploy13"
+    "DaimoPayHopBridger-deploy15"
 );
 bytes32 constant DEPLOY_SALT_LEGACY_MESH_BRIDGER = keccak256(
-    "DaimoPayLegacyMeshBridger-deploy7"
+    "DaimoPayLegacyMeshBridger-deploy9"
 );
 bytes32 constant DEPLOY_SALT_STARGATE_BRIDGER = keccak256(
-    "DaimoPayStargateBridger-deploy5"
+    "DaimoPayStargateBridger-deploy7"
 );
 
 // Relayer contract
